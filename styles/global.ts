4bb088a4004f65fbb23672de60d3e488379a529e
Thiago Lauter <thiagosantoslauter@gmail.com>
2021-11-08 15:31:00 -0300

    adding styled components

---
+++ styles/global.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  body {
+    color: black;
+    background-color: white;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 700;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
