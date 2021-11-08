821421bd0a909d3c1e9761eb52034f9a55af2e7c
Thiago Lauter <thiagosantoslauter@gmail.com>
2021-11-08 16:46:57 -0300

    testing getStaticProps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
+import GlobalStyles from '../styles/global';
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyles/>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
