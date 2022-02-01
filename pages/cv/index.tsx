73aefdb07e8e8659fb0250dd1d6aa21c30f27538
Thiago Lauter <thiagosantoslauter@gmail.com>
2022-02-01 15:07:27 +0000

    formating

--- pages/cv/index.tsx
+++ pages/cv/index.tsx
@@ -3,10 +3,10 @@ import type { NextPage } from 'next'
 const Cv: NextPage = () => {
     return (
         <>
-        <h1>cv</h1>
-        <p>I am bulding hehe.</p>
+            <h1>cv</h1>
+            <p>I am bulding hehe.</p>
         </>
-     )
+    )
 }
 
 export default Cv
