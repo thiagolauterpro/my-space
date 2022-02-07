fb68b2cda72e49cec304d7dc3d174f3f3ac8d0d0
Thiago Lauter <thiagosantoslauter@gmail.com>
2022-02-07 19:22:04 +0000

    Recreating footer

---
+++ components/Footer.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image"
+import styled from "styled-components"
+import github from '../public/icons/github-line.svg'
+import linkedin from '../public/icons/linkedin-line.svg'
+import instagram from '../public/icons/instagram-line.svg'
+import opensource from '../public/icons/open-source-line.svg'
+
+const Container = styled.div`
+  bottom: 0;
+  position: fixed;
+  
+  footer{
+    height: 10vh;
+    width: 100vw;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+
+  a{
+    display: flex;
+  }
+`;
+
+export default function Footer() {
+  return (
+    <Container>
+      <footer>
+        <a
+          href="https://github.com/thiagolauter"
+          target="_blank">
+          <Image src={github} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/thiagolauter/"
+          target="_blank">
+          <Image src={linkedin} />
+        </a>
+        <a
+          href="https://www.instagram.com/thiago_lauter/"
+          target="_blank">
+          <Image src={instagram} />
+        </a>
+        <a
+          href="https://github.com/thiagolauter/my-space"
+          target="_blank">
+          <Image src={opensource} />
+        </a>
+      </footer>
+    </Container>
+  );
+}
