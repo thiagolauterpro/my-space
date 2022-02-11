01f1627cdde63b736cd010287d35a14ae25d7931
Thiago Lauter <thiagosantoslauter@gmail.com>
2022-02-11 13:16:35 +0000

    ' to "

--- pages/cv/index.tsx
+++ pages/cv/index.tsx
@@ -2,19 +2,20 @@ import type { NextPage } from 'next'
 import styled from 'styled-components'
 
 const Cv: NextPage = () => {
-    return (
-        <Container>
-            <h1>cv</h1>
-            <p>Hey! I'm <span className='line-through'>walking</span> building here.</p>
-        </Container>
-    )
+  return (
+    <Container>
+      <h1>cv</h1>
+      <p>
+        Hey! I'm <span className="line-through">walking</span> building here.
+      </p>
+    </Container>
+  )
 }
 
 const Container = styled.main`
-    .line-through{
-        text-decoration: line-through;
-    }
+  .line-through {
+    text-decoration: line-through;
+  }
 `
 
 export default Cv
-
